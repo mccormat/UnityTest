fabc214f1754eb6de24c787e047c79c21ca87837
pdanner <paul.j.danner@gmail.com>
2014-03-27 15:53:47 -0500

    Updaed menu/boss - New menu and boss functionality

--- Tutorial/Assets/Scipts/MenuScript.cs
+++ Tutorial/Assets/Scipts/MenuScript.cs
@@ -18,20 +18,39 @@ public class MenuScript : MonoBehaviour
 		// Draw a button to start the game
 		if (
 			GUI.Button(
-			// Center in X, 2/3 of the height in Y
-			new Rect(
-			Screen.width / 2 - (buttonWidth / 2),
-			(2 * Screen.height / 3) - (buttonHeight / 2),
-			buttonWidth,
-			buttonHeight
-			),
-			"Start!"
-			)
-			)
-		{
+			// 1/3 from left in X, 2/3 of the height in Y
+				new Rect(Screen.width / 3 - (buttonWidth / 2),
+						(2 * Screen.height / 3) - (buttonHeight / 2),
+						buttonWidth,buttonHeight),
+				"Start!")
+		){
 			// On Click, load the first level.
 			// "Stage1" is the name of the first scene we created.
 			Application.LoadLevel("Stage1");
+		}
+		if (
+			GUI.Button(
+			// Center in X, 2/3 of the height in Y
+			new Rect(Screen.width / 2 - (buttonWidth / 2),
+		         (2 * Screen.height / 3) - (buttonHeight / 2),
+		         buttonWidth,buttonHeight),
+			"High Score")
+			){
+			// On Click, load the first level.
+			// "Stage1" is the name of the first scene we created.
+			Application.LoadLevel("Highscore");
+		}
+		if (
+			GUI.Button(
+			// 1/3 from right in X, 2/3 of the height in Y
+			new Rect(Screen.width / 1.5f - (buttonWidth / 2),
+		         (2 * Screen.height / 3) - (buttonHeight / 2),
+		         buttonWidth,buttonHeight),
+			"Settings")
+			){
+			// On Click, load the first level.
+			// "Stage1" is the name of the first scene we created.
+			Application.LoadLevel("Settings");
 		}
 	}
 }
